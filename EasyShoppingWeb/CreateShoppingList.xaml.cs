b847c9cb27689765e5e383b5c1b65e7598c1fd25
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2019-02-01 10:42:57 +0100

    Add project files.

---
+++ EasyShoppingWeb/CreateShoppingList.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyShoppingWeb
+{
+    /// <summary>
+    /// Interaction logic for CreateShoppingList.xaml
+    /// </summary>
+    public partial class CreateShoppingList : Window
+    {
+        public CreateShoppingList()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveList_Click(object sender, RoutedEventArgs e)
+        {
+            SavedList sL = new SavedList();
+            sL.Show();
+            this.Close();
+        }
+
+        private void AddToList_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+    }
+}
